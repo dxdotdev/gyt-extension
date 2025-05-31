b333a2f18333d13c31b738e12ca252dd9fcc330d
Davi Reis <dreisss.dev@gmail.com>
2025-05-30 23:30:23 -0300

    feat: implement ui

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -1,3 +1,32 @@
+import { Clipboard } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+
 export default function () {
-  return <div className="bg-black">kmsokdmasokdmas</div>
+  return (
+    <div className="flex flex-col gap-2 bg-background p-4">
+      <Select>
+        <SelectTrigger className="w-[200px]">
+          <SelectValue placeholder="Quality" />
+        </SelectTrigger>
+
+        <SelectContent>
+          <SelectItem value="hd">HD (1280x720)</SelectItem>
+          <SelectItem value="sd">SD (640x480)</SelectItem>
+          <SelectItem value="normal-lg">Normal (480x360)</SelectItem>
+          <SelectItem value="normal-md">Normal (320x180)</SelectItem>
+          <SelectItem value="normal-sm">Normal (120x90)</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <div className="flex gap-2">
+        <Button variant={'outline'}>Download</Button>
+
+        <Button variant={'outline'} size="icon">
+          <Clipboard />
+        </Button>
+      </div>
+    </div>
+  )
 }
