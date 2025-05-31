9c5f6e8f1df5e896ad47cb96916ffdb18bc3ee3a
Davi Reis <dreisss.dev@gmail.com>
2025-05-30 23:13:05 -0300

    feat: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -24,4 +25,9 @@ export default defineConfig({
       manifest: generateManifest,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
