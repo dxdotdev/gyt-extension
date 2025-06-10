095ab7eca72b4a44b091e43b3f21663dc963780d
Davi Reis <dreisss.dev@gmail.com>
2025-06-09 22:18:10 -0300

    chore: rollback to default react plugin instead of swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
 
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import webExtension, { readJsonFile } from 'vite-plugin-web-extension'
 
