39a21f9cb2226ea6a0d763c354871f8b9af07761
Davi Reis <dreisss.dev@gmail.com>
2025-06-10 00:46:01 -0300

    feat: start functionality working

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -1,32 +1,55 @@
 import { Clipboard } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import browser from 'webextension-polyfill'
 
 import { Button } from '@/components/ui/button'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 export default function () {
-  return (
-    <div className="flex flex-col gap-2 bg-background p-4">
-      <Select>
-        <SelectTrigger className="w-[200px]">
-          <SelectValue placeholder="Quality" />
-        </SelectTrigger>
+  const [isInYouTube, setIsInYouTube] = useState(false)
 
-        <SelectContent>
-          <SelectItem value="hd">HD (1280x720)</SelectItem>
-          <SelectItem value="sd">SD (640x480)</SelectItem>
-          <SelectItem value="normal-lg">Normal (480x360)</SelectItem>
-          <SelectItem value="normal-md">Normal (320x180)</SelectItem>
-          <SelectItem value="normal-sm">Normal (120x90)</SelectItem>
-        </SelectContent>
-      </Select>
+  useEffect(() => {
+    browser.tabs.query({ active: true, lastFocusedWindow: true }).then((tabs) => {
+      const tabUrl = tabs[0].url || ''
 
-      <div className="flex gap-2">
-        <Button variant={'outline'}>Download</Button>
+      setIsInYouTube(
+        /(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/.test(
+          tabUrl,
+        ),
+      )
+    })
+  }, [])
 
-        <Button variant={'outline'} size="icon">
-          <Clipboard />
-        </Button>
+  if (isInYouTube)
+    return (
+      <div className="flex flex-col gap-2 bg-background p-4">
+        <Select defaultValue="hd">
+          <SelectTrigger className="w-[200px]">
+            <SelectValue placeholder="Quality" />
+          </SelectTrigger>
+
+          <SelectContent>
+            <SelectItem value="hd">HD (1280x720)</SelectItem>
+            <SelectItem value="sd">SD (640x480)</SelectItem>
+            <SelectItem value="normal-lg">Normal (480x360)</SelectItem>
+            <SelectItem value="normal-md">Normal (320x180)</SelectItem>
+            <SelectItem value="normal-sm">Normal (120x90)</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <div className="flex gap-2">
+          <Button variant={'outline'}>Download</Button>
+
+          <Button variant={'outline'} size="icon">
+            <Clipboard />
+          </Button>
+        </div>
       </div>
+    )
+
+  return (
+    <div className="grid aspect-video w-[200px] place-items-center">
+      <p>Invalid YouTube url</p>
     </div>
   )
 }
