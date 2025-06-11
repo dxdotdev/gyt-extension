1a73562fc3c3dbb51f0213ff323515da2b6e8b57
Davi Reis <dreisss.dev@gmail.com>
2025-06-11 14:15:04 -0300

    feat: wip main functionality implementation

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -5,51 +5,53 @@ import browser from 'webextension-polyfill'
 import { Button } from '@/components/ui/button'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
+const YT_REGEX = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
+
 export default function () {
   const [isInYouTube, setIsInYouTube] = useState(false)
+  const [videoId, setVideoId] = useState('')
+  const [imageQuality, setImageQuality] = useState('mqdefault')
 
   useEffect(() => {
     browser.tabs.query({ active: true, lastFocusedWindow: true }).then((tabs) => {
       const tabUrl = tabs[0].url || ''
-
-      setIsInYouTube(
-        /(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/.test(
-          tabUrl,
-        ),
-      )
+      setIsInYouTube(YT_REGEX.test(tabUrl))
+      setVideoId(YT_REGEX.exec(tabUrl)?.at(5) || '')
     })
   }, [])
 
-  if (isInYouTube)
+  const downloadUrl = `https://img.youtube.com/vi/${videoId}/${imageQuality}.jpg`
+  browser.downloads.download({ url: downloadUrl, filename: 'Teste' })
+
+  if (!isInYouTube)
     return (
-      <div className="flex flex-col gap-2 bg-background p-4">
-        <Select defaultValue="hd">
-          <SelectTrigger className="w-[200px]">
-            <SelectValue placeholder="Quality" />
-          </SelectTrigger>
-
-          <SelectContent>
-            <SelectItem value="hd">HD (1280x720)</SelectItem>
-            <SelectItem value="sd">SD (640x480)</SelectItem>
-            <SelectItem value="normal-lg">Normal (480x360)</SelectItem>
-            <SelectItem value="normal-md">Normal (320x180)</SelectItem>
-            <SelectItem value="normal-sm">Normal (120x90)</SelectItem>
-          </SelectContent>
-        </Select>
-
-        <div className="flex gap-2">
-          <Button variant={'outline'}>Download</Button>
-
-          <Button variant={'outline'} size="icon">
-            <Clipboard />
-          </Button>
-        </div>
+      <div className="grid aspect-video w-[200px] place-items-center">
+        <p className="text-muted-foreground text-xs">Enter in a valid YouTube video.</p>
       </div>
     )
 
   return (
-    <div className="grid aspect-video w-[200px] place-items-center">
-      <p>Invalid YouTube url</p>
+    <div className="flex w-[200px] flex-col gap-2 bg-background p-4">
+      <Select defaultValue="mqdefault" defaultOpen>
+        <SelectTrigger className="w-full">
+          <SelectValue placeholder="Quality" />
+        </SelectTrigger>
+
+        <SelectContent>
+          <SelectItem value="maxresdefault">HD (1280x720)</SelectItem>
+          <SelectItem value="sddefault">SD (640x480)</SelectItem>
+          <SelectItem value="mqdefault">Normal (320x180)</SelectItem>
+          <SelectItem value="default">Normal (120x90)</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <div className="flex gap-2">
+        <Button variant={'outline'}>Download</Button>
+
+        <Button variant={'outline'} size="icon">
+          <Clipboard />
+        </Button>
+      </div>
     </div>
   )
 }
