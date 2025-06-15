315327f54ff3679dabeec1605dcca121d5698515
Davi Reis <dreisss.dev@gmail.com>
2025-06-14 21:55:46 -0300

    refactor: add react hook form

--- src/pages/Popup.tsx
+++ src/pages/Popup.tsx
@@ -2,30 +2,46 @@ import { Clipboard } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import browser from 'webextension-polyfill'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import { Button } from '@/components/ui/button'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Form, FormField } from '@/components/ui/form'
 
 const YT_REGEX = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
 
-export default function () {
-  const [isInYouTube, setIsInYouTube] = useState(false)
-  const [videoId, setVideoId] = useState('')
-  const [imageQuality, setImageQuality] = useState('mqdefault')
+const formSchema = z.object({
+  quality: z.enum(['maxresdefault', 'sddefault', 'mqdefault', 'default']),
+  filename: z.string(),
+})
+
+export default function Popup() {
+  const [videoId, setVideoId] = useState<string | undefined>()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      quality: 'default',
+      filename: 'thumbnail',
+    },
+  })
 
   useEffect(() => {
     browser.tabs.query({ active: true, lastFocusedWindow: true }).then((tabs) => {
       const tabUrl = tabs[0].url || ''
-      setIsInYouTube(YT_REGEX.test(tabUrl))
-      setVideoId(YT_REGEX.exec(tabUrl)?.at(5) || '')
+
+      if (YT_REGEX.test(tabUrl)) setVideoId(YT_REGEX.exec(tabUrl)?.at(5))
     })
   }, [])
 
-  function handleDownload() {
-    const downloadUrl = `https://img.youtube.com/vi/${videoId}/${imageQuality}.jpg`
-    browser.downloads.download({ url: downloadUrl, filename: 'default.jpg' })
+  function handleDownload(values: z.infer<typeof formSchema>) {
+    const downloadUrl = `https://img.youtube.com/vi/${videoId}/${values.quality}.jpg`
+    browser.downloads.download({ url: downloadUrl, filename: `${values.filename}.jpg` })
   }
 
-  if (!isInYouTube)
+  if (!videoId)
     return (
       <div className="grid aspect-video w-[200px] place-items-center">
         <p className="text-muted-foreground text-xs">Enter in a valid YouTube video.</p>
@@ -33,29 +49,37 @@ export default function () {
     )
 
   return (
-    <div className="flex w-[200px] flex-col gap-2 bg-background p-4">
-      <Select defaultValue="mqdefault" defaultOpen>
-        <SelectTrigger className="w-full">
-          <SelectValue placeholder="Quality" />
-        </SelectTrigger>
+    <Form {...form}>
+      <form className="flex w-[200px] flex-col gap-2 bg-background p-4" onSubmit={form.handleSubmit(handleDownload)}>
+        <FormField
+          control={form.control}
+          name="quality"
+          render={({ field }) => (
+            <Select onValueChange={field.onChange} defaultValue={field.value}>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Quality" />
+              </SelectTrigger>
 
-        <SelectContent>
-          <SelectItem value="maxresdefault">HD (1280x720)</SelectItem>
-          <SelectItem value="sddefault">SD (640x480)</SelectItem>
-          <SelectItem value="mqdefault">Normal (320x180)</SelectItem>
-          <SelectItem value="default">Normal (120x90)</SelectItem>
-        </SelectContent>
-      </Select>
+              <SelectContent>
+                <SelectItem value="maxresdefault">HD (1280x720)</SelectItem>
+                <SelectItem value="sddefault">SD (640x480)</SelectItem>
+                <SelectItem value="mqdefault">Normal (320x180)</SelectItem>
+                <SelectItem value="default">Normal (120x90)</SelectItem>
+              </SelectContent>
+            </Select>
+          )}
+        />
 
-      <div className="flex gap-2">
-        <Button variant={'outline'} onClick={handleDownload}>
-          Download
-        </Button>
+        <div className="flex gap-2">
+          <Button variant={'outline'} type="submit">
+            Download
+          </Button>
 
-        <Button variant={'outline'} size="icon">
-          <Clipboard />
-        </Button>
-      </div>
-    </div>
+          <Button variant={'outline'} size="icon">
+            <Clipboard />
+          </Button>
+        </div>
+      </form>
+    </Form>
   )
 }
